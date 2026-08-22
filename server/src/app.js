5ba897826daa154f7ebe8690bd4a0345aba971f2
MUKESHBOLISETTY <mukeshbolisetty09@gmail.com>
2026-08-22 15:08:03 +0530

    initial commit

---
+++ server/src/app.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import helmet from 'helmet';
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(helmet());
+
+const corsoptions = {
+    origin: process.env.origin,
+    methods: "GET, POST, PUT, DELETE, HEAD, PATCH",
+    credentials: true,
+};
+app.use(cors(corsoptions));
+app.set('trust proxy', 1);
+
+app.get('/', (req, res) => {
+    return res.json({
+        success: true,
+        message: "Your server is up and running",
+    });
+});
+
+app.use((err, req, res, next) => {
+    if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+        return res.status(400).json({
+            success: false,
+            message: "Bad Request: Malformed JSON"
+        });
+    }
+    res.status(500).json({
+        success: false,
+        message: "An unexpected error occurred on the server."
+    });
+});
+
+export default app;
